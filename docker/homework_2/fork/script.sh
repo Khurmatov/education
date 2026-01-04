960cd495d0ed603c044fdf6321618bb29a74891d
khurmatov <khurmatov.r.i@yandex.ru>
2026-01-04 19:07:29 +0500

    Задание 4

--- docker/homework_2/fork/script.sh
+++ docker/homework_2/fork/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/Khurmatov/education.git"
-TARGET_DIR="/opt/docker/homework_2/fork"
+TARGET_DIR="/opt"
 
 if [ -d "$TARGET_DIR" ]; then
   echo "Обновление репозитория..."
-  cd $TARGET_DIR
+  cd /opt/docker/homework_2/fork
   git pull
 else
   echo "Клонирование репозитория..."
   git clone $REPO_URL $TARGET_DIR
-  cd $TARGET_DIR
+  cd /opt/docker/homework_2/fork
 fi
 
 echo "Запуск проекта..."
