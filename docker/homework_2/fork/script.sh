59852366d236b917e391aede0c0a5e1a4a33e4a4
khurmatov <khurmatov.r.i@yandex.ru>
2026-01-05 09:28:40 +0500

    Задание 4

--- docker/homework_2/fork/script.sh
+++ docker/homework_2/fork/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/Khurmatov/education.git"
-TARGET_DIR="/opt/docker/homework_2/fork"
+TARGET_DIR="/opt/education"
 
 if [ -d "$TARGET_DIR" ]; then
   echo "Обновление репозитория..."
