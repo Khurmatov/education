808b36adf5aa3e9c81ee2e8f2151475b3a2399a7
khurmatov <khurmatov.r.i@yandex.ru>
2026-01-09 16:45:07 +0500

    terraform

--- ter-homeworks/02/src/main.tf
+++ ter-homeworks/02/src/main.tf
@@ -10,15 +10,15 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_image_name
+  family = "ubuntu-2004-lts"
 }
 resource "yandex_compute_instance" "platform" {
-  name            = var.vm_web_name
-  platform_id     = var.vm_web_platform_id
+  name        = "netology-develop-platform-web"
+  platform_id = "standard-v3"
   resources {
-    cores         = var.vm_web_resources.cores
-    memory        = var.vm_web_resources.memory
-    core_fraction = var.vm_web_resources.core_fraction
+    cores         = 2
+    memory        = 1
+    core_fraction = 20
   }
   boot_disk {
     initialize_params {
