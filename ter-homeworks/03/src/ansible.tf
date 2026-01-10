2414d6146173dc82c9c69bf5f5f7f4282ca431e0
khurmatov <khurmatov.r.i@yandex.ru>
2026-01-10 18:41:52 +0500

    terraform

---
+++ ter-homeworks/03/src/ansible.tf
@@ -0,0 +1,9 @@
+resource "local_file" "inventory" {
+  content = templatefile("${path.module}/hosts.tftpl",
+    {
+      webservers = yandex_compute_instance.web
+      databases  = yandex_compute_instance.database
+      storage    = [yandex_compute_instance.storage]
+    })
+  filename = "${abspath(path.module)}/hosts.cfg"
+}
